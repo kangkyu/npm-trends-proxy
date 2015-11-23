44a9ae317005644e4ce3637a09e98ccfa4a3f678
John Potter <johnpotter1440@gmail.com>
2015-11-23 11:14:48 -0800

    added proxy for all api requests

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 'use strict';
 
 var Woden = require('woden'),
-		woden = new Woden({}),
-		DS = {};
+		woden = new Woden({
+			changeOrigin: true
+		}),
+		redis = require('redis'),
+		client = redis.createClient({'return_buffers': true});
+
+client.select(10);
+
+woden.when( /.*/, {
+	cacheTimeout: function( cacheEntry, req, proxyRes ) {
+    if ( cacheEntry.body.length > 10000000 ) {
+      return -1; // don't cache big responses
+    }
+
+    if ( proxyRes.statusCode === 404 ) {
+      return -1; // don't cache 404 responses
+    }
+
+    return 86400; // cache for 24 hours (in seconds for redis)
+  }  
+})
+
+woden.store({ 
+  get: function( key, callback ) {
+    client.get(key, function(err, reply){
+    	callback(err, JSON.parse(reply));
+    });
+  },
+  set: function( data, callback ) {
+    client.set(data.key, JSON.stringify(data.value), function(err, reply){
+    	callback();
+    });
+  }
+});
 
 woden.listen(4444);
 console.log('Listening on 4444...');
